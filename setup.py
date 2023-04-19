1dc9efbed89e266e7adbdbb447b61e6f94ae1d2b
Ajeet Yadav <ajeetyadav164@gmail.com>
2023-04-19 17:04:02 +0530

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.5"
-DESCRIPTION = "CLI AWS MFA"
-LONG_DESCRIPTION = "Use MFA to increase the security of your AWS environment. Signing in with MFA requires an authentication code from an MFA device. Each user can have a maximum of 8 MFA devices assigned"
+VERSION = "0.0.6"
+DESCRIPTION = "AWS MFA - Manage Session Token easily"
+LONG_DESCRIPTION = "Use MFA to increase the security of your AWS environment. Signing in with MFA requires an authentication code from an MFA device."
 
 # Setting up
 setup(
